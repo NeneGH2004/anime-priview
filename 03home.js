4c592d4ddb275f0f7413f610792924aa6729c7ce
NeneGH2004 <qiushangzhou456@gmail.com>
2026-03-28 15:04:31 +0800

    更新-260328
    1.优化跳转功能逻辑
    2.ui界面优化

---
+++ 03home.js
@@ -0,0 +1,6 @@
+var home = {
+  template: `
+  <div class="container mt-2">
+    <h3 class="text-danger">首页热门动漫</h3>
+  </div>`,
+}
---
+++ 05upcoming.js
@@ -0,0 +1,6 @@
+var upcoming = {
+  template: `
+  <div class="container mt-2">
+    <h3 class="text-danger">即将开播</h3>
+  </div>`,
+}
