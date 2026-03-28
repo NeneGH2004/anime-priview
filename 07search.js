dcca3f028ea252585ece8c4ed708b75f48877dbb
NeneGH2004 <qiushangzhou456@gmail.com>
2026-03-28 15:06:55 +0800

    Delete 07search.js

--- 07search.js
+++
@@ -1,7 +0,0 @@
-var search = {
-    template: `
-     <div class="container mt-2">
-        <h3 class="text-danger">搜索结果</h3>
-        <p class="text-muted">为你查找「{{$parent.searchtxt}}」相关的动漫预告片</p>
-    </div>`
-}
